d255473df1d75a43181ef2c0aa29f05f64e07a26
Crisleicon Francisco <crisleicon@yahoo.com.br>
2020-03-27 16:02:55 -0300

    Validação com celebrate

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,8 +8,7 @@ const app = express();
 app.use(cors());
 //informando ao express que no corpo da requisição será usado o formato json
 app.use(express.json());
-
 app.use(routes);
-
+app.use(errors());
 
 app.listen(3333);
